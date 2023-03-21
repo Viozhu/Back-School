20a4c4b5b9ff4ecf83a210d79230b3dd223fee9f
Viozhu <nachog_man@icloud.com>
2023-03-21 23:41:55 +0900

    fix: 1

--- src/routes/User/index.ts
+++ src/routes/User/index.ts
@@ -1,38 +1,35 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 
 import {
   getUsers,
   getUser,
   createUser,
   updateUser,
-  deleteUser
-} from '../../controller/index'
+  deleteUser,
+} from '../../controller/index';
 
-const router = express.Router()
+const router = express.Router();
 
 router.get(
   '/getUsers',
-  async (req: Request, res: Response) =>
-    await getUsers(req, res)
-)
+  async (req: Request, res: Response) => await getUsers(req, res),
+);
 router.get(
   '/getUser/:id',
-  async (req: Request, res: Response) => await getUser(req, res)
-)
+  async (req: Request, res: Response) => await getUser(req, res),
+);
 router.post(
   '/create',
-  async (req: Request, res: Response) =>
-    await createUser(req, res)
-)
+  async (req: Request, res: Response) => await createUser(req, res),
+);
 router.post(
   '/update',
-  async (req: Request, res: Response) =>
-    await updateUser(req, res)
-)
+  async (req: Request, res: Response) => await updateUser(req, res),
+);
 router.delete(
   '/delete/:id',
   async (req: Request, res: Response): Promise<void> =>
-    await deleteUser(req, res)
-)
+    await deleteUser(req, res),
+);
 
-export default router
+export default router;
