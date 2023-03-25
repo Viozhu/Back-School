947d5011bcf10243ea819bf751f33dbd3b50522e
Viozhu <nachog_man@icloud.com>
2023-03-25 11:10:44 +0900

    fix

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -25,8 +25,6 @@ export const errorHandlerRequest = (
   res.status(500).send({
     success: false,
     status: 500,
-    error: JSON.parse(
-      JSON.stringify(err.message.replaceAll('\n', "'").replaceAll("'", ''))
-    )
+    error: JSON.parse(JSON.stringify(err.message))
   })
 }
